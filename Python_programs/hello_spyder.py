2f8d42ca26a22075be52296dfd243a7eb31524b5
Arnab Mitra <arnabmitra471@gmail.com>
2026-01-03 22:47:10 +0530

    Learnt about strings and string methods in python

---
+++ Python_programs/hello_spyder.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 31 09:09:03 2025
+
+@author: User
+"""
+
+def hello_spyder():
+    print("Hi I am Arnab Mitra")
+    print("This is my first ever program in spyder IDE")
+    print("It is a very interactive,powerful and feature rich IDE")
+    
+hello_spyder()
+
+name = "Arnab Mitra"
+num1 = 25
+num2 = 24
+num3 = 45.78
+print(num1 + num2)
+print(num1 + num3)
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(name))
